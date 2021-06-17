91c8281e52fba958c1bf1c95470da267b27aa7ca
tioburla <gabrielrochagbl@gmail.com>
2021-06-16 20:16:19 -0400

    BringUp AOSKP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,35 +1,8 @@
-# LineageOS (crDroid)
+# AOSKP
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_santoni.mk
+    $(LOCAL_DIR)/aosp_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_santoni-userdebug \
-    lineage_santoni-user \
-    lineage_santoni-eng
-
-# HavocOS
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/havoc_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    havoc_santoni-userdebug \
-    havoc_santoni-user \
-    havoc_santoni-eng
-
-# AOSiP
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/aosip_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    aosip_santoni-userdebug \
-    aosip_santoni-user \
-    aosip_santoni-eng
-
-# Bliss
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/bliss_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    bliss_santoni-userdebug \
-    bliss_santoni-user \
-    bliss_santoni-eng
+    aosp_santoni-userdebug \
+    aosp_santoni-user \
+    aosp_santoni-eng
